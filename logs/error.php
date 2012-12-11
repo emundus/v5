aa6ff90cb19c75ebcef2a112b74b0b84708a7443
Benjamin Rivalland <benjamin@decisionpublique.fr>
2012-12-11 19:25:16 +0100

    delete configuration.php

--- logs/error.php
+++ logs/error.php
@@ -30,3 +30,4 @@
 2012-12-05	12:23:09	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2012-12-05	12:23:21	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2012-12-05	12:23:48	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-12-11	18:17:36	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
