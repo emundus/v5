81dce7e802a021aef5ad2795d77b30c5b44a153c
Benjamin Rivalland <benjamin@decisionpublique.fr>
2012-12-11 20:24:45 +0100

    fabrik update

--- logs/error.php
+++ logs/error.php
@@ -31,3 +31,4 @@
 2012-12-05	12:23:21	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2012-12-05	12:23:48	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2012-12-11	18:17:36	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-12-11	18:37:39	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
