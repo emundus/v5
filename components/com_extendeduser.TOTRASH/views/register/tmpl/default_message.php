cccf80dc19bd3f38d611968bc05f7c47e7ad4141
Benjamin Rivalland <benjamin@decisionpublique.fr>
2013-02-21 15:40:19 +0100

    cleanup

--- components/com_extendeduser.TOTRASH/views/register/tmpl/default_message.php
+++
@@ -1,9 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<fieldset style="background-color:#FC0">
-<h2><?php echo $this->message->title ; ?></h2>
-
-<div class="message">
-	<?php echo  $this->message->text ; ?>
-</div>
-</fieldset>
